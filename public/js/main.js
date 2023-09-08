70a8b3507c91d325182fc887faa4f044c80e114c
Ariel Antequiera <arielantequiera@gmail.com>
2023-09-07 22:40:09 -0300

    Se agrega función de carrito de compras

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
+const cartMenu = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail2');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+cartMenu.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+      aside.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
-}
+  }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+      aside.classList.add('inactive'); 
+    }
+    
     mobileMenu.classList.toggle('inactive');
+  }
+
+  function toggleCartAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive'); 
+    }
+    
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive'); // Agregar esta línea para ocultar desktopMenu
 }
